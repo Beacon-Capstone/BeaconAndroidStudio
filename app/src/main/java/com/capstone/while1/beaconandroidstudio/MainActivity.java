7ea4d80ab3ff29c059fddb8237a3ba62d0f5d4b8
Addison Payden <acpppf@mail.missouri.edu>
2017-03-18 19:53:06 -0500

    Minor commit

--- app/src/main/java/com/capstone/while1/beaconandroidstudio/MainActivity.java
+++ app/src/main/java/com/capstone/while1/beaconandroidstudio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.capstone.while1.beaconandroidstudio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        startActivity(new Intent(getApplicationContext(), MapsActivity.class));
     }
 }
