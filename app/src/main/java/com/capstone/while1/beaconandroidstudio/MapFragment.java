6f1bbb5106480b1274e8f210f479528d204735ae
Addison Payden <acpppf@mail.missouri.edu>
2017-03-23 16:10:06 -0500

    Minor Commit, reworking security settings so it properly checks if permissions are set.  Working on splash screen.

--- app/src/main/java/com/capstone/while1/beaconandroidstudio/MapFragment.java
+++ app/src/main/java/com/capstone/while1/beaconandroidstudio/MapFragment.java
@@ -1,30 +1,35 @@
 package com.capstone.while1.beaconandroidstudio;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.LocationSource.OnLocationChangedListener;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMyLocationButtonClickListener,
+        OnLocationChangedListener {
 
     MapView mMapView;
     private GoogleMap googleMap;
+    private LocationManager lm;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,27 +50,69 @@ public class MapFragment extends Fragment {
             @Override
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
-
-                // For showing a move to my location button
-                if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                googleMap.setMyLocationEnabled(true);
-
-                // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition.Builder builder = new CameraPosition.Builder();
-                builder.target(sydney);
-                builder.zoom(12);
-                CameraPosition cameraPosition = builder.build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                enableLocation();
             }
         });
 
         return rootView;
+    }
+
+    private void enableLocation() {
+        if (googleMap != null) {
+            try {
+                googleMap.setMyLocationEnabled(true);
+
+                lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
+                    @SuppressWarnings("MissingPermission")
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        getLocation(location);
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                });
+                Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                getLocation(l);
+            } catch (SecurityException ex) {
+                Toast.makeText(getActivity(), "Test", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void getLocation(Location location) {
+        if (location == null) {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+            String provider = lm.getBestProvider(criteria, true);
+
+            //noinspection MissingPermission
+            location = lm.getLastKnownLocation(provider);
+        }
+
+        if (location != null) {
+            LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 16), 1500, null);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
     }
 
     @Override
@@ -90,5 +137,10 @@ public class MapFragment extends Fragment {
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        getLocation(location);
     }
 }
