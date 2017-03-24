afd86bd9b1d8b85f314c74b5a6f0b71313faf82a
Addison Payden <acpppf@mail.missouri.edu>
2017-03-24 11:59:49 -0500

    Minor Commit, reworking security settings so it properly checks if permissions are set.  Working on splash screen.

--- app/src/main/java/com/capstone/while1/beaconandroidstudio/SplashActivity.java
+++ app/src/main/java/com/capstone/while1/beaconandroidstudio/SplashActivity.java
@@ -3,16 +3,11 @@ package com.capstone.while1.beaconandroidstudio;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat.OnRequestPermissionsResultCallback;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
 /**
  * Created by AP047572 on 3/24/2017.
@@ -23,31 +18,11 @@ public class SplashActivity extends AppCompatActivity implements OnRequestPermis
      * The code used when requesting permissions
      */
     private static final int PERMISSIONS_REQUEST = 1234;
-    private int timeoutMillis = 2000;
-    private long startTimeMillis = 0;
-
-    public int getTimeoutMillis() {
-        return timeoutMillis;
-    }
+    private boolean mPermissionDenied = false;
 
     @SuppressWarnings("rawtypes")
     public Class getNextActivityClass() {
         return MainActivity.class;
-    }
-
-    public String[] getRequiredPermissions() {
-        String[] permissions = null;
-        try {
-            permissions = getPackageManager().getPackageInfo(getPackageName(),
-                    PackageManager.GET_PERMISSIONS).requestedPermissions;
-        } catch (NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (permissions == null) {
-            return new String[0];
-        } else {
-            return permissions.clone();
-        }
     }
 
     @TargetApi(23)
@@ -55,14 +30,14 @@ public class SplashActivity extends AppCompatActivity implements OnRequestPermis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        startTimeMillis = System.currentTimeMillis();
-
         /**
          * On a post-Android 6.0 devices, check if the required permissions have
          * been granted.
          */
-        if (Build.VERSION.SDK_INT >= 23) {
-            checkPermissions();
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            PermissionUtils.requestPermission(this, PERMISSIONS_REQUEST, android.Manifest.permission.ACCESS_FINE_LOCATION, true);
         } else {
             startNextActivity();
         }
@@ -72,13 +47,13 @@ public class SplashActivity extends AppCompatActivity implements OnRequestPermis
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         if (requestCode == PERMISSIONS_REQUEST) {
-            for (int i = 0, len = permissions.length; i < len; i++) {
-                String permission = permissions[i];
-                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
-                    finish();
-                }
+            if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Enable the my location layer if the permission has been granted.
+                startNextActivity();
+            } else {
+                mPermissionDenied = true;
             }
-            startNextActivity();
         }
     }
 
@@ -87,34 +62,20 @@ public class SplashActivity extends AppCompatActivity implements OnRequestPermis
         finish();
     }
 
-    @TargetApi(23)
-    private void checkPermissions() {
-        String[] ungrantedPermissions = requiredPermissionsStillNeeded();
-        if (ungrantedPermissions.length == 0) {
-            startNextActivity();
-        } else {
-            requestPermissions(ungrantedPermissions, PERMISSIONS_REQUEST);
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            showMissingPermissionError();
+            mPermissionDenied = false;
         }
     }
 
-    @TargetApi(23)
-    private String[] requiredPermissionsStillNeeded() {
-
-        Set<String> permissions = new HashSet<String>();
-        for (String permission : getRequiredPermissions()) {
-            permissions.add(permission);
-        }
-        for (Iterator<String> i = permissions.iterator(); i.hasNext(); ) {
-            String permission = i.next();
-            if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
-                Log.d(SplashActivity.class.getSimpleName(),
-                        "Permission: " + permission + " already granted.");
-                // TODO: Add dialog saying this permission is needed to run
-                i.remove();
-            }
-        }
-        return permissions.toArray(new String[permissions.size()]);
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
-
-
 }
