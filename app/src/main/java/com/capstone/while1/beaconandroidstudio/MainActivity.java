cf7a3480495bc00e3e2fa4d680939f0b199373d1
Aaron Whaley <acw365@mail.missouri.edu>
2017-04-12 17:42:14 -0500

    Connected main and settings activities

--- app/src/main/java/com/capstone/while1/beaconandroidstudio/MainActivity.java
+++ app/src/main/java/com/capstone/while1/beaconandroidstudio/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+//Push
+
 
 public class MainActivity extends AppCompatActivity {
 
