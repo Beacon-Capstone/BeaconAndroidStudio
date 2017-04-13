fb02114be38359a39b449fe31aee835bd9651dc1
QuestionableAntics <questionableantics.km@gmail.com>
2017-04-12 20:11:53 -0500

    a

--- app/src/main/java/com/capstone/while1/beaconandroidstudio/SettingsActivity.java
+++ app/src/main/java/com/capstone/while1/beaconandroidstudio/SettingsActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+//import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 
 public class SettingsActivity extends PreferenceActivity {
@@ -36,11 +38,15 @@ public class SettingsActivity extends PreferenceActivity {
         @Override
         public void onOptionsMenuClosed(Menu menu) {
             SharedPreferences sp = getActivity().getPreferences(Context.MODE_PRIVATE);
+//            SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
             SharedPreferences.Editor editor = sp.edit();
+            Log.v("abc", String.valueOf(R.id.wifiBool));
+            Log.v("abc", String.valueOf(R.id.eventRadius));
+            Log.v("abc", String.valueOf(R.id.eventRefreshInterval));
             editor.putInt("wifiBool", R.id.wifiBool);
             editor.putFloat("eventRadius", R.id.eventRadius);
             editor.putFloat("eventRefreshInterval", R.id.eventRefreshInterval);
-            editor.putString("startingLocation", R.id.startingLocation);
+//            editor.putString("startingLocation", R.id.startingLocation);
             editor.apply();
         }
 
