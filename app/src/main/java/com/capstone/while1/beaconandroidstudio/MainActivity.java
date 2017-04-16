7aa6433b2cd6e79a2bf928522cf1f3b1c33ce413
HomerGaidarski <homer.gaidarski@gmail.com>
2017-04-15 20:02:34 -0500

    radius slider and cancel button for add event dialog is functional

--- app/src/main/java/com/capstone/while1/beaconandroidstudio/MainActivity.java
+++ app/src/main/java/com/capstone/while1/beaconandroidstudio/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                View dialogView = getLayoutInflater().inflate(R.layout.dialog_add_event, null);
+                final View dialogView = getLayoutInflater().inflate(R.layout.dialog_add_event, null);
+
+                //placeholder for create event logic
                 final EditText eventName = (EditText) dialogView.findViewById(R.id.createEventName);
                 Button createButton = (Button) dialogView.findViewById(R.id.createEventButton);
                 createButton.setOnClickListener(new View.OnClickListener(){
@@ -45,8 +49,46 @@ public class MainActivity extends AppCompatActivity {
                         eventName.setText("U SUCK AT HANZO SWITCH!");
                     }
                 });
+
+                //seekbar stuff for radius
+                final TextView textView = (TextView)dialogView.findViewById(R.id.createEventSeekBarText);
+                SeekBar seekBar = (SeekBar)dialogView.findViewById(R.id.createEventSeekBar);
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    int progressValue = 0;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progressValue = progress + 1;
+                        textView.setText(" Radius (Miles): " + (progress + 1));
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        //textView.setText(" Radius (Miles): " + progressValue);
+                    }
+                });
+
+
+
                 builder.setView(dialogView);
-                AlertDialog dialog = builder.create();
+
+                final AlertDialog dialog = builder.create();
+
+                //cancel button, closes add event dialog, not needed but nice to have
+                Button cancelButton = (Button) dialogView.findViewById(R.id.cancelEventButton);
+                cancelButton.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+
                 dialog.show();
             }
         });
