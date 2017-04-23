aff5aff2b30bf5b22a7aeaa43f767a2a206fbb67
Jared Melton <joetemporary@outlook.com>
2017-04-23 00:00:51 -0500

    Added registration functionality.

--- app/src/main/java/com/capstone/while1/beaconandroidstudio/RegisterActivity.java
+++ app/src/main/java/com/capstone/while1/beaconandroidstudio/RegisterActivity.java
@@ -8,7 +8,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.capstone.while1.beaconandroidstudio.beacondata.BeaconConsumer;
 import com.capstone.while1.beaconandroidstudio.beacondata.BeaconData;
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -28,12 +33,14 @@ public class RegisterActivity extends AppCompatActivity {
         EditText emailInput = (EditText) findViewById(R.id.emailInput);
         EditText passInput = (EditText) findViewById(R.id.passwordInput);
         EditText verifyPassInput = (EditText) findViewById(R.id.verifyPasswordInput);
-        TextView messageOutput = (TextView) findViewById(R.id.messageOutput);
+        final TextView messageOutput = (TextView) findViewById(R.id.messageOutput);
 
-        String username = userInput.getText().toString();
-        String password = passInput.getText().toString();
-        String email = emailInput.getText().toString();
-        String verifyPassword = verifyPassInput.getText().toString();
+        final String username = userInput.getText().toString();
+        final String password = passInput.getText().toString();
+        final String email = emailInput.getText().toString();
+        final String verifyPassword = verifyPassInput.getText().toString();
+
+        final RegisterActivity currentContext = this;
 
         if (! password.equals(verifyPassword)) {
             messageOutput.setText("Registration failed: password fields don't match.");
@@ -41,6 +48,40 @@ public class RegisterActivity extends AppCompatActivity {
         }
         else {
             // Verify passed
+
+            // Make sure the username isn't taken
+            BeaconData.isValidUsername(username, new BeaconConsumer<Boolean>() {
+                @Override
+                public void accept(Boolean isTaken) {
+                    if (! isTaken) {
+                        // The username is not taken!!!
+                        // Create the account!
+                        BeaconData.createUser(username, email, password,
+                                new Runnable() {
+                                    // Successfully created the user!
+                                    @Override
+                                    public void run() {
+                                        BeaconData.registerLogin(currentContext, username, password);
+                                        Intent intent = new Intent(currentContext, MainActivity.class);
+                                        currentContext.startActivity(intent);
+                                    }
+                                },
+                                new BeaconConsumer<String>() {
+                                    @Override
+                                    public void accept(String obj) {
+                                        // Failed to create the user...
+                                        messageOutput.setText("Registration failed.");
+                                    }
+                                });
+                    }
+                    else {
+                        // Username has been taken
+                        messageOutput.setText("Registration failed: Username has been taken.");
+                    }
+                }
+            });
+
+            // Create the user!
 
         }
     }
